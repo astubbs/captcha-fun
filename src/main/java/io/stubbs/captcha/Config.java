740f0900c2e8a097a712d379d5633307f0aa1c38
Antony Stubbs <antony.stubbs@gmail.com>
2020-05-07 19:48:17 +0100

    Config & memory images

---
+++ src/main/java/io/stubbs/captcha/Config.java
@@ -0,0 +1,6 @@
+package io.stubbs.captcha;
+
+public class Config {
+    static String ocadoDataSiteKey = "6LcRDbsUAAAAAP8Kg4CtjPzIY40yzlgwzXFV4JzV"; // data-sitekey
+    static String apiKey = "YOUR 2CAPCHA KEY";
+}
--- src/main/java/io/stubbs/selenium/SeleniumUtils.java
+++ src/main/java/io/stubbs/selenium/SeleniumUtils.java
@@ -1,5 +1,6 @@
 package io.stubbs.selenium;
 
+import com.google.common.base.Stopwatch;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
@@ -9,10 +10,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.util.Base64;
 
+import static org.openqa.selenium.OutputType.BYTES;
 import static org.openqa.selenium.OutputType.FILE;
 
 @Slf4j
@@ -27,10 +30,15 @@ public class SeleniumUtils {
         // Get entire page screenshot
         // TODO screenshot to memory buffer instead of disk
         log.info("Screenshot whole page...");
-        File screenshot = driver.getScreenshotAs(FILE);
+        //File screenshot = driver.getScreenshotAs(FILE);
         // String screenshotAs = driver.getScreenshotAs(BASE64);
-        log.info("Read screenshot off disk...");
-        BufferedImage fullImg = ImageIO.read(screenshot);
+        Stopwatch w = Stopwatch.createStarted();
+        byte[] screenshot = driver.getScreenshotAs(BYTES);
+        log.info(w.elapsed().toString());
+        BufferedImage fullImg = ImageIO.read(new ByteArrayInputStream(screenshot));
+        log.info(w.elapsed().toString());
+//        log.info("Read screenshot off disk...");
+        // BufferedImage fullImg = ImageIO.read(screenshot);
 
         int scale = 2;
 
@@ -53,7 +61,9 @@ public class SeleniumUtils {
                 scaledHeight,
                 BufferedImage.TYPE_INT_RGB);
         log.info("Scaling image...");
+        log.info(w.elapsed().toString());
         scaledImage.createGraphics().drawImage(eleScreenshot, 0, 0, scaledWidth, scaledHeight, Color.WHITE, null);
+        log.info(w.elapsed().toString());
 
 
         // Copy the element screenshot to disk
@@ -66,6 +76,7 @@ public class SeleniumUtils {
         // File screenshotLocation = File.createTempFile("screenshot-part", ".png");
 
         log.info("Saving scaled image to disk...");
+        log.info(w.elapsed().toString());
         boolean jpg1 = ImageIO.write(scaledImage, "jpg", screenshotLocationjpg);
         if (!jpg1) throw new RuntimeException("Failed to find writer...");
 
@@ -76,12 +87,15 @@ public class SeleniumUtils {
         // BufferedOutputStream output = new BufferedOutputStream(scaledOutputBuffer);
         log.info("Save ...");
         boolean jpg = ImageIO.write(scaledImage, "jpg", scaledOutputBuffer);
+        log.info(w.elapsed().toString());
         if (!jpg) throw new RuntimeException("Failed to find writer...");
 
         scaledOutputBuffer.close();
 
         log.info("Base64 encoding...");
+        log.info(w.elapsed().toString());
         String base64EncodedImage = Base64.getEncoder().encodeToString(scaledOutputBuffer.toByteArray());
+        log.info(w.elapsed().toString());
         // FileUtils.copyFile(screenshot, screenshotLocation);
         return base64EncodedImage;
     }
